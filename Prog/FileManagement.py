5298253388b379927f76bc3683b7dc62ecb32ce4
Geordi Jean-Luc Collette <g@collette-online.nl>
2018-05-27 12:13:10 +0200

    first steps towards gui
    :-1: baaaaaaaaaaad
    will get better though :+1:

--- Prog/FileManagement.py
+++ Prog/FileManagement.py
@@ -4,3 +4,9 @@ import fileinput
 
 def load(x):
     x = 10
+
+
+"""
+Better to make this a C binding,
+C would be much more efficient in dealing with files and memory operations with such.
+"""
--- Prog/UI.py
+++ Prog/UI.py
@@ -1,17 +1,30 @@
 import tkinter
-import random
-from math import floor
+#import random
+#from math import floor
 
 class UI:
 
-    def __init__(self):
+    def __init__(self, master):
         """self.ID = floor(float(random.randint(1,999999)/29189) * 10000000000)
         if len(str(self.ID)) < 12:
             self.ID = self.ID * 10 ** (12-len(str(self.ID)))
         print(self.ID)"""
         self.title = "Can Do Flow"
         self.createUI()
+        
 
     def createUI(self):
+        print("Nothing")
 
-        self.f = tkinter.Canvas
+
+root = tkinter()
+
+UI(root)
+
+root.mainloop()
+
+"""
+Animations are optional,
+however I'm not sure as to wether these will work anyway.
+But functionality is more important than eyecandy at this stage.
+"""
